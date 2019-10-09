ebe78384a95a430385a8a9181062a6a846a11b33
luz.paz <luzpaz@users.noreply.github.com>
2019-10-09 09:22:22 -0400

    sortDictionariesAndCleanup.sh: Enforce sorting 'LC_ALL=en_US' to avoid portability issues.

--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -47,3 +47,8 @@ cat ${WORKINGDIR}/dictionary_tmp.txt | sort | uniq > ${WORKINGDIR}dictionary_all
 sed 's/[[:blank:]]*$//' -i ${WORKINGDIR}/dictionary_all.txt
 # convert upper case to lower case letters
 sed 's/\([A-Z]\)/\L\1/g' -i ${WORKINGDIR}/dictionary_all.txt
+# Remove duplicate entries -i e.g.: abc->abc
+sed -E '/^(.*)->\1$/d' -i ${WORKINGDIR}/dictionary_all.txt
+# Sort and remove duplicate entries
+cat ${WORKINGDIR}/dictionary_all.txt | LC_ALL=en_US sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt 
+mv ${WORKINGDIR}/dictionary_tmp.txt ${WORKINGDIR}/dictionary_all.txt 
